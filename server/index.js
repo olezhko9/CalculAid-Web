4552567b45991ba5555a8884686e6c523559e6f1
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-11-28 00:41:59 +0300

    removed nuxt from server

--- server/index.js
+++ server/index.js
@@ -1,36 +1,20 @@
-const configNuxt = require('../nuxt.config')
 const Koa = require('koa')
 const koaBody = require('koa-body')
-const { Nuxt, Builder } = require('nuxt')
+const cors = require('@koa/cors')
+const ip = require('ip')
 
-// import router from './router'
 const router = require('./router')
 
 async function start() {
   const app = new Koa()
-  const host = 'localhost'
+
+  const host = ip.address()
   const port = 3000
 
-  configNuxt.dev = !(app.env === 'production')
-
-  const nuxt = new Nuxt(configNuxt)
-
-  if (configNuxt.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
-
+  app.use(cors());
   app.use(koaBody({multipart: true}));
   app.use(router.routes())
   app.use(router.allowedMethods())
-
-
-  app.use(ctx => {
-    ctx.status = 200
-    ctx.respond = false
-    ctx.req.ctx = ctx
-    nuxt.render(ctx.req, ctx.res)
-  })
 
   app.listen(port, host)
   console.log('Server listening on ' + host + ':' + port)
