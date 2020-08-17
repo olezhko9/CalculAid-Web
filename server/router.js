5181d572e8bce85bd134def4e6522ee0754376d5
Oleg Naumov <oleg.naumov.98@mail.ru>
2020-08-17 22:20:37 +0300

    fix request body parse

--- server/router.js
+++ server/router.js
@@ -3,11 +3,13 @@ const router = new Router({ prefix: '/api' })
 const speechToProducts = require('./speech-to-products')
 
 router.post('/products', async (ctx) => {
-  const speech = JSON.parse(ctx.request.body).speech;
+  const speech = ctx.request.body.speech;
+  console.log(speech);
   const data = await speechToProducts(speech)
   ctx.body = {
     data
   }
+  ctx.status = 200
 })
 
 module.exports = router
