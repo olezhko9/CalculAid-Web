e09a1835af8cc63453bf946fa4c60676b612472e
Oleg Naumov <oleg.naumov.98@mail.ru>
2020-08-18 23:42:25 +0300

    try to add https via greenlock module

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 const cors = require('@koa/cors')
 const ip = require('ip')
+const https = require('https');
 
 const router = require('./router')
 
 async function start() {
+
+  const greenlock = require('greenlock-koa').create({
+    version: 'draft-11',
+    server: 'https://acme-v02.api.letsencrypt.org/directory',
+    email: 'oleg.naumov.98@mail.ru',
+    agreeTos: true,
+    approveDomains: [ 'calculaid.tk' ],
+    communityMember: false,
+    configDir: require('os').homedir() + '/acme/etc'
+  });
+
   const app = new Koa()
 
   const host = ip.address()
@@ -16,8 +28,8 @@ async function start() {
   app.use(router.routes())
   app.use(router.allowedMethods())
 
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port)
+  const server = https.createServer(greenlock.tlsOptions, greenlock.middleware(app.callback()));
+  server.listen(3000, () => {console.log('Server listening on ' + host + ':' + port)})
 }
 
 start()
